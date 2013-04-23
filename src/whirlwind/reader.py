746baeae148af8223c8a28febb07f40cd2c7d0a2
Mathieu Lecarme <mlecarme@bearstech.com>
2013-04-23 21:51:25 +0200

    Seek.

--- src/whirlwind/reader.py
+++ src/whirlwind/reader.py
@@ -21,6 +21,12 @@ class ReadOnlyFileStream(BaseIOStream):
             return None
         return chunk
 
+    def seek(self, pos, callback):
+        os.lseek(self.fileno(), pos, os.SEEK_SET)
+        self._read_bytes = 0
+        self._read_buffer.clear()
+        self._set_read_callback(callback)
+
 
 if __name__ == "__main__":
     from tornado.ioloop import IOLoop
@@ -31,7 +37,11 @@ if __name__ == "__main__":
         f = ReadOnlyFileStream('./README.md')
         first = yield Task(f.read_until, "\n")
         deuz = yield Task(f.read_until, "\n")
+        yield Task(f.seek, 0)
+        refirst = yield Task(f.read_until, "\n")
         f.close()
+        print first, refirst
+        assert first == refirst
         print first, deuz
 
     IOLoop.instance().run_sync(main)
