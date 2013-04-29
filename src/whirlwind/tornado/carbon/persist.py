9613e1cdced4ea41c1eacf801d26e913aa8f338e
Mathieu Lecarme <mlecarme@bearstech.com>
2013-04-29 23:40:44 +0200

    Metrics its own metrics.

--- src/whirlwind/tornado/carbon/persist.py
+++ src/whirlwind/tornado/carbon/persist.py
@@ -4,12 +4,14 @@ import os.path
 import os
 
 import whisper
-from redis import Redis
+from redis import StrictRedis as Redis
 
 from whirlwind import target_to_path
 
 METRICS = 'metrics'
 PERIOD = 30
+METRIC_WRITE = 'carbon.write'
+METRIC_POINTS = 'carbon.points'
 
 
 class Persist(object):
@@ -18,16 +20,29 @@ class Persist(object):
         self.redis = Redis()
         self.path = path
         self.dirs = set()
+        self.redis.sadd(METRICS, METRIC_POINTS, METRIC_WRITE)
+
+    def metric(self, name, value):
+        timestamp = time.time()
+        serialized = struct.pack('!ff', timestamp, value)
+        pipe = self.redis.pipeline()
+        pipe.zadd(name, timestamp, serialized)
+        pipe.publish(name, serialized)
+        pipe.execute()
 
     def run(self):
         while True:
-            tick = int(time.time())
+            before = time.time()
             self.handle()
-            time.sleep(PERIOD - tick + int(time.time()))
+            after = time.time()
+            self.metric(METRIC_WRITE, (after - before) * 1000)
+            time.sleep(PERIOD - int(before) + int(after))
 
     def handle(self):
+        points = 0
         for metric in self.redis.smembers(METRICS):
             values = self.redis.zrange(metric, 0, -1)
+            points += len(values)
             f = target_to_path(self.path, metric)
             d = os.path.dirname(f)
             if d not in self.dirs:
@@ -37,8 +52,10 @@ class Persist(object):
             if not os.path.exists(f):
                 whisper.create(f, [(10, 1000)])  # [FIXME] hardcoded values
             whisper.update_many(f, [struct.unpack('!ff', a) for a in values])
+            print "Writing", metric
             if len(values):
                 self.redis.zrem(metric, *values)
+        self.metric(METRIC_POINTS, points)
 
 if __name__ == "__main__":
     p = Persist()
