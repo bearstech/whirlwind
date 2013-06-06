f0212895b24fbdb06ff8fb4d90a682b2b1a5e61b
Mathieu Lecarme <mlecarme@bearstech.com>
2013-06-06 12:02:08 +0200

    Splitting tornado code.

--- setup.py
+++ setup.py
@@ -2,17 +2,14 @@ from distutils.core import setup
 
 
 setup(name='whirlwind',
-      version='0.1',
+      version='0.2',
       author='Mathieu Lecarme',
       author_email='mlecarme@bearstech.com',
       license='BSD',
       package_dir = {'': 'src'},
-      packages= ['whirlwind', 'whirlwind.tornado', 'whirlwind.tornado.carbon', 'whirlwind.tornado.whisper'],
-      scripts=['scripts/whirlwind'],
+      packages= ['whirlwind'],
       requires=[
-          'tornado(>=3.0)',
           'whisper',
           'pyparsing(==1.5.7)',
-          'pytz',
-          'flask']
+          'pytz']
       )
