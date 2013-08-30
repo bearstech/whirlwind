fb5a694f1939a73faf720595928dffe5ceee2502
Mathieu Lecarme <mlecarme@bearstech.com>
2013-08-30 22:07:54 +0200

    PEP8.

--- src/whirlwind/grammar.py
+++ src/whirlwind/grammar.py
@@ -7,15 +7,15 @@ expression = Forward()
 
 # Literals
 intNumber = Combine(
-  Optional('-') + Word(nums)
+    Optional('-') + Word(nums)
 )('integer')
 
 floatNumber = Combine(
-  Optional('-') + Word(nums) + Literal('.') + Word(nums)
+    Optional('-') + Word(nums) + Literal('.') + Word(nums)
 )('float')
 
 sciNumber = Combine(
-  (floatNumber | intNumber) + CaselessLiteral('e') + intNumber
+    (floatNumber | intNumber) + CaselessLiteral('e') + intNumber
 )('scientific')
 
 aString = quotedString('string')
@@ -23,14 +23,14 @@ aString = quotedString('string')
 # Use lookahead to match only numbers in a list (can't remember why this is necessary)
 afterNumber = FollowedBy(",") ^ FollowedBy(")") ^ FollowedBy(LineEnd())
 number = Group(
-  (sciNumber + afterNumber) |
-  (floatNumber + afterNumber) |
-  (intNumber + afterNumber)
+    (sciNumber + afterNumber) |
+    (floatNumber + afterNumber) |
+    (intNumber + afterNumber)
 )('number')
 
 boolean = Group(
-  CaselessKeyword("true") |
-  CaselessKeyword("false")
+    CaselessKeyword("true") |
+    CaselessKeyword("false")
 )('boolean')
 
 argname = Word(alphas + '_', alphanums + '_')('argname')
@@ -55,10 +55,10 @@ backslash = Literal('\\').suppress()
 
 symbols = '''(){},=.'"\\'''
 arg = Group(
-  boolean |
-  number |
-  aString |
-  expression
+    boolean |
+    number |
+    aString |
+    expression
 )('args*')
 kwarg = Group(argname + equal + arg)('kwargs*')
 
@@ -66,32 +66,32 @@ args = delimitedList(~kwarg + arg)  # lookahead to prevent failing on equals
 kwargs = delimitedList(kwarg)
 
 call = Group(
-  funcname + leftParen +
-  Optional(
-    args + Optional(
-      comma + kwargs
-    )
-  ) + rightParen
+    funcname + leftParen +
+    Optional(
+        args + Optional(
+            comma + kwargs
+        )
+    ) + rightParen
 )('call')
 
 # Metric pattern (aka. pathExpression)
 validMetricChars = ''.join((set(printables) - set(symbols)))
 escapedChar = backslash + Word(symbols, exact=1)
 partialPathElem = Combine(
-  OneOrMore(
-    escapedChar | Word(validMetricChars)
-  )
+    OneOrMore(
+        escapedChar | Word(validMetricChars)
+    )
 )
 
 matchEnum = Combine(
-  leftBrace +
-  delimitedList(partialPathElem, combine=True) +
-  rightBrace
+    leftBrace +
+    delimitedList(partialPathElem, combine=True) +
+    rightBrace
 )
 
 pathElement = Combine(
-  Group(partialPathElem | matchEnum) +
-  ZeroOrMore(matchEnum | partialPathElem)
+    Group(partialPathElem | matchEnum) +
+    ZeroOrMore(matchEnum | partialPathElem)
 )
 pathExpression = delimitedList(pathElement, delim='.', combine=True)('pathExpression')
 
@@ -100,9 +100,9 @@ grammar << expression
 
 
 def enableDebug():
-  for name,obj in globals().items():
-    try:
-      obj.setName(name)
-      obj.setDebug(True)
-    except:
-      pass
+    for name,obj in globals().items():
+        try:
+            obj.setName(name)
+            obj.setDebug(True)
+        except:
+            pass
