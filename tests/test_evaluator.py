0238cc80f9d04bbe9673a15d1f59385b9d9d77a4
Mathieu Lecarme <mlecarme@bearstech.com>
2013-08-30 22:34:33 +0200

    Min and Max.

--- tests/test_evaluator.py
+++ tests/test_evaluator.py
@@ -1,6 +1,6 @@
 import unittest
 from whirlwind import evaluator
-from whirlwind.mock import MockStoreStatic
+from whirlwind.mock import MockStoreStatic, MockStore
 
 
 class EvaluatorTest(unittest.TestCase):
@@ -31,3 +31,21 @@ class EvaluatorTest(unittest.TestCase):
         #values = self._evaluator('diffSeries(a.b.5,3)')
         #for v in values[0]:
             #assert v == 2.0
+
+    def test_min_max(self):
+        store = MockStore({'a.b.c': [1, 2, 3, 4, 1, 5],
+                           'd.e.f': [2, 1, 3, 0, 6, 7]
+                           })
+        context = {
+            'startTime': '-2days',
+            'endTime': 'now',
+        }
+        tokens = 'minSeries(a.b.c, d.e.f)'
+        values = evaluator.evaluateTarget(store, context, tokens)
+        vv = [v for v in values[0] if v is not None]
+        assert vv == [1, 1, 3, 0, 1, 5]
+
+        tokens = 'maxSeries(a.b.c, d.e.f)'
+        values = evaluator.evaluateTarget(store, context, tokens)
+        vv = [v for v in values[0] if v is not None]
+        assert vv == [2, 2, 3, 4, 6, 7]
