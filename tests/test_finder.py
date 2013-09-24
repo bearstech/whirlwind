ae721442afbd1ed61ae5befb5281f07054d9f0f9
Mathieu Lecarme <mlecarme@bearstech.com>
2013-09-24 12:05:08 +0200

    Decent finder test.

--- tests/test_finder.py
+++ tests/test_finder.py
@@ -1,3 +1,4 @@
+import time
 import unittest
 
 from whirlwind.mock import MockFinder, MockNoiseReader
@@ -10,5 +11,12 @@ class FinderTest(unittest.TestCase):
         r = MockNoiseReader()
         m = MockFinder({'mock.a.b.c': r})
         f = FindQuery('mock.a.*', None, None)
+        now = int(time.time())
         for node in m.find_nodes(f):
             assert node.path == 'mock.a.b.c'
+            timeInfo, values = node.fetch(now - 300, now)
+            startTime, endTime, step = timeInfo
+            assert startTime == now - 300
+            assert endTime == now
+            assert step == 30  # Default value
+            assert len(values) == (endTime - startTime) / step
